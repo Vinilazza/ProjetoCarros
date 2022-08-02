abdc32dc2c467e1452380ce629e0add1675e4a81
Vinicius Lazzaroto <vinilazza2016cris@gmail.com>
2022-08-02 14:00:46 -0300

    Estrutura basica montada

--- js/index.js
+++ js/index.js
@@ -1,5 +1,35 @@
-function search() {
-  var input,filter,ul,li,a,i,txtValue;
-  input = document.querySelector("#myInput");
-  filter = input.value.toUpperCase();
+const menuBtn = document.querySelector(".menu-icon span");
+const searchBtn = document.querySelector(".search-icon");
+const cancelBtn = document.querySelector(".cancel-icon");
+const items = document.querySelector(".nav-items");
+const form = document.querySelector("form");
+
+menuBtn.addEventListener('click', () => {
+  items.classList.add("active");
+  menuBtn.classList.add("hide");
+  searchBtn.classList.add("hide");
+  cancelBtn.classList.add("show");
+})
+cancelBtn.onclick = () => {
+  items.classList.remove("active");
+  menuBtn.classList.remove("hide");
+  searchBtn.classList.remove("hide");
+  cancelBtn.classList.remove("show");
+  form.classList.remove("active");
+  cancelBtn.style.color = "#ff3d00";
 }
+searchBtn.onclick = () => {
+  form.classList.add("active");
+  searchBtn.classList.add("hide");
+  cancelBtn.classList.add("show");
+}
+
+document.addEventListener( 'DOMContentLoaded', function () {
+  new Splide( '#thumbnail-carousel', {
+		fixedWidth : 100,
+    fixedHeight: 60,
+		gap        : 10,
+		rewind     : true,
+		pagination : false,
+  } ).mount();
+} );
