91e3c53f750a44909e8b6a5fc4eced69028cf22f
Vinicius Lazzaroto <vinilazza2016cris@gmail.com>
2022-08-03 09:37:50 -0300

    Update(Escolha o modelo)

--- js/index.js
+++ js/index.js
@@ -24,3 +24,25 @@ searchBtn.onclick = () => {
   cancelBtn.classList.add("show");
 }
 
+
+$('.dropdown').click(function () {
+  $(this).attr('tabindex', 1).focus();
+  $(this).toggleClass('active');
+  $(this).find('.dropdown-menu').slideToggle(300);
+});
+$('.dropdown').focusout(function () {
+  $(this).removeClass('active');
+  $(this).find('.dropdown-menu').slideUp(300);
+});
+$('.dropdown .dropdown-menu li').click(function () {
+  $(this).parents('.dropdown').find('span').text($(this).text());
+  $(this).parents('.dropdown').find('input').attr('value', $(this).attr('id'));
+});
+/*End Dropdown Menu*/
+
+
+$('.dropdown-menu li').click(function () {
+  let input2 = document.querySelector(".dropdown input").value;
+  let msg = document.querySelector('.msg').innerHTML = `O modelo escolhido foi <a class="text-decoration-none fw-bold" style="color:coral;" href="${input2}">${input2}</a>`;
+  document.querySelector('.dropdown input').style.display = '';
+}); 
