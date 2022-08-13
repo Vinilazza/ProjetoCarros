42f0e0da9567d505a0e3aeb383bb66bd7bd05b97
Vinilazza <108952051+Vinilazza@users.noreply.github.com>
2022-08-13 12:21:58 -0300

    Updated AdminDashbord

--- api/conexaoBD.js
+++ api/conexaoBD.js
@@ -2,14 +2,37 @@ var mysql = require('mysql');
 
 var con = mysql.createConnection({
   host: "localhost",
-  user: "myusername",
-  password: "mypassword",
-  database: "mydb"
+  user: "root",
+  password: "vinicris"
 });
+
+/* CREATE NA TABELA */
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });
+});
+
+
+/* INSERT NA TABELA */
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
+});
+
+/* SELECT NA TABELA */
 
 con.connect(function(err) {
   if (err) throw err;
-  //Select all customers and return the result object:
   con.query("SELECT * FROM customers", function (err, result, fields) {
     if (err) throw err;
     console.log(result);
